db08c3a9c580d4d0fc619c6f15cb6c16097a2828
Rodrigo Almeida <rodrigozalmeida@gmail.com>
2024-07-16 22:23:13 -0300

    add Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("curso JAVA");
+        curso1.setDescricao("descrição do curso");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso JS");
+        curso2.setDescricao("descrição do JS");
+        curso2.setCargaHoraria(4);
+
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria JAVA");
+        mentoria.setDescricao("descrição mentoria JAVA");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
